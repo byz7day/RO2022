938a08b4de87f4fdbf03e6b0cc58ada49449928f
null <muramaza.prosever@live.com>
2022-05-11 15:02:57 +0700

    Update serverupdate.bat

--- serverupdate.bat
+++ serverupdate.bat
@@ -2,5 +2,9 @@
 ECHO Getting the latest Git from the null webservice
 Echo Wait...
 git pull https://github.com/il3ol2ed/rathena_webservice.git
+git pull https://github.com/rathena/rathena.git update/4th_improvement_bundle
+git pull https://github.com/rathena/rathena.git script/episode_17_2
+git pull https://github.com/limitro/rathena.git NightWatchSkills
+
 Echo ---------------------------------------------------
 Set /p Wait="Finished."
